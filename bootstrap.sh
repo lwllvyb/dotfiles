d793e6d50361fdb6164947340133a1852ca37cf9
liwenlong <liwenlong03@sankuai.com>
2018-11-27 16:49:05 +0800

    remove bin dir

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,7 @@
 #! /usr/bin/env bash
 
 ETC=~/.local/etc
-BIN=~/.local/bin
 mkdir -p $ETC
-mkdir -p $BIN
 
 # git clone respository
 mkdir -p .tmp
@@ -12,7 +10,6 @@ rm -rf dotfiles
 git clone https://github.com/lwllvyb/dotfiles.git
 cd dotfiles
 cp -rf etc/* $ETC/
-cp -rf bin/* $BIN/
 
 # source init.sh
 sed -i "\:$ETC/init.sh:d" ~/.bashrc
