666d7b2fba4997d5a54a3f99fec6b20d2b586707
liwenlong03 <mr.liwenlong@outlook.com>
2020-03-06 17:37:42 +0800

    revert etc setup.sh

--- etc/setup.sh
+++ etc/setup.sh
@@ -7,33 +7,32 @@ cd "$DIR"
 
 SOURCE="$(realpath ./)"
 APP="shell"
-shell_format() {
-    local env=$1
-    local shfmt_bin=./bin/shfmt
-    if [ -f "$shfmt_bin" ]; then
-        $shfmt_bin -version >/dev/null 2>&1
-        if [ "$?" == "0"]; then
-            success ""
-            return
-        fi
-    fi
-    rm -f $shfmt_bin
-    if [ "$env" == "Darwin" ]; then
-        wget -q https://github.com/mvdan/sh/releases/download/v3.0.2/shfmt_v3.0.2_darwin_amd64 $shfmt_bin
-
-    fi
-}
 
 info "Setting up $APP..."
-install() {
-    if [ "$(uname)" == "Darwin" ]; then
-    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-        # Do something unde GNU/Linux platform
-        echo '1'
-    else
-        error "Can't detect operation environment $(uname -s)"
-        return 1
-    fi
-}
+
+if [ "$(uname)" == "Darwin" ]; then
+	substep_info "Installing bash..."
+	sed -i '' "\:$SOURCE/init.sh:d" $HOME/.bashrc
+	echo ". $SOURCE/init.sh" >> $HOME/.bashrc
+	substep_info "Finished installing bash."
+	substep_info "Installing zsh..."
+	sed -i '' "\:$SOURCE/init.sh:d" $HOME/.zshrc
+	echo ". $SOURCE/init.sh" >> $HOME/.zshrc
+	substep_info "Finished installing zsh."
+    # Do something unde Mac OS X platform
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	substep_info "Installing bash..."
+	sed -i "\:$SOURCE/init.sh:d" $HOME/.bashrc
+	echo ". $SOURCE/init.sh" >> $HOME/.bashrc
+	substep_info "Finished installing bash."
+	substep_info "Installing zsh..."
+	sed -i "\:$SOURCE/init.sh:d" $HOME/.zshrc
+	echo ". $SOURCE/init.sh" >> $HOME/.zshrc
+	substep_info "Finished installing zsh."
+    # Do something unde GNU/Linux platform
+else
+	error "Can't detect operation environment $(uname -s)"
+	exit 1
+fi
 
 success "Finished setting up $APP."
