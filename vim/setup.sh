1523483209f71ede79a78ddbb5ed9fd221e8472a
liwenlong03 <mr.liwenlong@outlook.com>
2020-03-06 18:06:12 +0800

    fix

--- vim/setup.sh
+++ vim/setup.sh
@@ -14,6 +14,7 @@ substep_info "Installing vim8..."
 mv $HOME/.vimrc $HOME/.vimrc.bak.$(date +"%Y%m%d_%H%M")
 symlink "$SOURCE/vimrc" "$HOME/.vimrc"
 symlink "$SOURCE/coc-settings.json" "$HOME/.vim/coc-settings.json"
+symlink "$SOURCE/package.json" "$HOME/.config/coc/extensions/package.json"
 substep_info "Finished installing vim8."
 
 substep_info "Installing nvim..."
@@ -21,6 +22,10 @@ substep_info "Installing nvim..."
 mkdir -p $HOME/.config/nvim
 symlink "$SOURCE/vimrc" "$HOME/.config/nvim/init.vim"
 symlink "$SOURCE/coc-settings.json" "$HOME/.config/nvim/coc-settings.json"
+symlink "$SOURCE/package.json" "$HOME/.config/coc/extensions/package.json"
 substep_info "Finished installing nvim."
+
+# install coc use script
+./coc.sh
 
 success "Finished setting up $APP."
