3369d1c99240d320d686bc3ca60aecd27be386f3
liwenlong03 <liwenlong03@meituan.com>
2020-03-18 02:51:49 +0000

    promot

--- etc/setup.sh
+++ etc/setup.sh
@@ -9,40 +9,41 @@ SOURCE="$(realpath ./)"
 APP="shell"
 
 info "Setting up $APP..."
+scripts=(init.sh z.sh promot_256.sh)
 
 if [ "$(uname)" == "Darwin" ]; then
-	substep_info "Installing bash..."
-	sed -i '' "\:$SOURCE/init.sh:d" $HOME/.bashrc
-	echo ". $SOURCE/init.sh" >> $HOME/.bashrc
-	sed -i '' "\:$SOURCE/z.sh:d" $HOME/.bashrc
-	echo ". $SOURCE/z.sh" >> $HOME/.bashrc
-	substep_info "Finished installing bash."
+	substep_info "Installing bash for Mac..."
+	for script in ${scripts[@]}; do
+		sed -i '' "\:$SOURCE/${script}:d" $HOME/.bashrc
+		echo ". $SOURCE/${script}" >> $HOME/.bashrc
+	done
+	substep_info "Finished installing bash for Mac."
 
 	if [ -f "$HOME/.zshrc" ]; then
-		substep_info "Installing zsh..."
-		sed -i '' "\:$SOURCE/init.sh:d" $HOME/.zshrc
-		echo ". $SOURCE/init.sh" >> $HOME/.zshrc
-		sed -i '' "\:$SOURCE/z.sh:d" $HOME/.zshrc
-		echo ". $SOURCE/z.sh" >> $HOME/.zshrc
-		substep_info "Finished installing zsh."
+		substep_info "Installing zsh for Mac..."
+		for script in ${scripts[@]}; do
+			sed -i '' "\:$SOURCE/${script}:d" $HOME/.bashrc
+			echo ". $SOURCE/${script}" >> $HOME/.bashrc
+		done
+		substep_info "Finished installing zsh for Mac."
 	fi
     # Do something unde Mac OS X platform
 elif [ "$(uname -s)" == "Linux" ]; then
-	substep_info "Installing bash..."
-	sed -i "\:$SOURCE/init.sh:d" $HOME/.bashrc
-	echo ". $SOURCE/init.sh" >> $HOME/.bashrc
-	sed -i "\:$SOURCE/z.sh:d" $HOME/.bashrc
-	echo ". $SOURCE/z.sh" >> $HOME/.bashrc
-	substep_info "Finished installing bash."
+	substep_info "Installing bash for Linux..."
+	for script in ${scripts[@]}; do
+		sed -i "\:$SOURCE/${script}:d" $HOME/.bashrc
+		echo ". $SOURCE/${script}" >> $HOME/.bashrc
+	done
+	substep_info "Finished installing bash for Linux."
+
 	if [ -f "$HOME/.zshrc" ]; then
-		substep_info "Installing zsh..."
-		sed -i "\:$SOURCE/init.sh:d" $HOME/.zshrc
-		echo ". $SOURCE/init.sh" >> $HOME/.zshrc
-		sed -i "\:$SOURCE/z.sh:d" $HOME/.zshrc
-		echo ". $SOURCE/z.sh" >> $HOME/.zshrc
-		substep_info "Finished installing zsh."
+		substep_info "Installing zsh for Linux..."
+		for script in ${scripts[@]}; do
+			sed -i "\:$SOURCE/${script}:d" $HOME/.bashrc
+			echo ". $SOURCE/${script}" >> $HOME/.bashrc
+		done
+		substep_info "Finished installing zsh for Linux."
 	fi
-    # Do something unde GNU/Linux platform
 else
 	error "Can't detect operation environment $(uname -s)"
 	exit 1
