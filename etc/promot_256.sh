c7fa9a37360188bf8adf719d74037c3e3b954e70
liwenlong03 <liwenlong03@meituan.com>
2020-03-20 04:01:26 +0000

    bugfix

--- etc/promot_256.sh
+++ etc/promot_256.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ -n "$BASH_VERSION" ]; then
-    export PS1='\[\e[38;5;135m\]\u\[\e[0m\]@\[\e[38;5;166m\]\h\[\e[0m\] \[\e[38;5;118m\]\w\[\e[0m\] \$ '
+    export PS1='\[\e[38;5;135m\]\u\[\e[0m\]@\[\e[38;5;166m\]\h\[\e[0m\] \[\e[38;5;118m\]\w\[\e[0m\] 
+\$ '
 else
     if [ "$UID" -eq 0 ]; then
         export PROMPT="%F{135}%n%f@%F{166}%m%f %F{118}%~%f %
