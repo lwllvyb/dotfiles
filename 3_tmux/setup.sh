7c55a7b006ba7fa468a33404472598b07b89212d
liwenlong <mr.liwenlong@outlook.com>
2021-04-07 16:45:11 +0800

    update tmux

--- 3_tmux/setup.sh
+++ 3_tmux/setup.sh
@@ -7,5 +7,5 @@ TMUX_GIT=${CURRENT_DIR_AB}/tmux/
 APP="tmux"
 
 info "Setting up $APP..."
-ln -s ${TMUX_GIT}/.tmux.conf  ~/
+ln -s ${TMUX_GIT}/tmux.conf  ~/.tmux.conf
 success "Finished setting up $APP."
