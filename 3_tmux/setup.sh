3f648f2fad239f5f281a7e44594e4067e0cd23f5
liwenlong <mr.liwenlong@outlook.com>
2021-04-14 11:15:09 +0800

    update tmux

--- 3_tmux/setup.sh
+++ 3_tmux/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. ../scripts/functions.sh
+source ../scripts/functions.sh
 CURRENT_DIR_AB="$(realpath ./)"
 TMUX_GIT=${CURRENT_DIR_AB}/tmux/
 
@@ -8,12 +8,13 @@ APP="tmux"
 
 info "Setting up $APP..."
 #---- install tmux plugin ------------------------------------
-[ -d ~/.tmux/plugins ] || mkdir -p ~/.tmux/plugins
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+[ -d $HOME/.tmux/plugins ] || mkdir -p $HOME/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 
-ln -s ${TMUX_GIT}/tmux.conf  ~/.tmux.conf
+rm $HOME/.tmux.conf
+ln -s ${TMUX_GIT}/tmux.conf  $HOME/.tmux.conf
 
 # install plugins
-~/.tmux/plugins/tpm/scripts/install_plugins.sh
+$HOME/.tmux/plugins/tpm/scripts/install_plugins.sh
 
 success "Finished setting up $APP."
